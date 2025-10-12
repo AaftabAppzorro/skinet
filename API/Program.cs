354754636f6d8ce7a9c07ff961871169ecf60aed
AaftabAppzorro <AaftabAppzorro@gmail.com>
2025-10-12 20:15:47 +0530

    Section 4: The Specification Pattern

--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,23 @@
 using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//SERVICES
 // Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-
-//MIDDLEWAREs
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 
 app.MapControllers();
 
